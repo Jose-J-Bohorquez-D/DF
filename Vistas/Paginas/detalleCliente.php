8970fbbbd4ffe4d6d9801e5d4efabcc29769ebc1
Jose Bohorquez <bd567358546@gmail.com>
2022-06-17 22:21:12 -0500

    se crea y modulariza la vista del tab del perfil del cliente es decir donde se muestra la info dl cliente , se deja listo para el muestreo de datos

--- Vistas/Paginas/detalleCliente.php
+++ Vistas/Paginas/detalleCliente.php
@@ -1,6 +1,4 @@
-  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
@@ -20,25 +18,16 @@
       <div class="container-fluid">
         <div class="row">
           <div class="col-md-3">
-
-            <!-- Profile Image -->
             <div class="card card-primary card-outline">
               <div class="card-body box-profile">
                 <h3 class="profile-username text-center">nombre empresa</h3>
-
                 <p class="text-muted text-center">nombre dueño</p>
                 <a href="#" class="btn btn-primary btn-block"><b>editar perfil cliente</b></a>
               </div>
             </div>
-
-
           </div>
-
-
           <div class="col-md-9">
-
             <div class="card">
-
               <div class="card-header p-2">
                 <ul class="nav nav-pills">
                   <li class="nav-item"><a class="nav-link active" href="#perfil" data-toggle="tab">perfil</a></li>
@@ -48,39 +37,30 @@
                   <li class="nav-item"><a class="nav-link" href="#alcohol" data-toggle="tab">Alcohol and drugs</a></li>
                 </ul>
               </div>
-
-
               <div class="card-body">
                 <div class="tab-content">
-
                   <div class="active tab-pane" id="perfil">
                   <!-- espacio para mostrar datos del cliente -->
-                  <p>espacio para mostrar datos del cliente</p>                 
+                  <?php require_once("Vistas/Paginas/detalleClienteTabPerfil.php"); ?>                
                   </div>
-
                   <div class="tab-pane" id="trucks">
                   <!-- espacio para mostrar vehiculos del cliente -->
                   <p>espacio para mostrar vehiculos del cliente</p>  
                   </div>
-
                   <div class="tab-pane" id="files">
                   <!-- espacio para mostrar los archivos subidos de este cliente -->
                   <p>espacio para mostrar los archivos subidos de este cliente</p> 
                   </div>
-
                   <div class="tab-pane" id="pdf">
                   <!-- espacio para mostrar los archivos subidos de este cliente --> 
                   <p>espacio para mostrar los formatos pdf subidos de este cliente</p>
                   </div>
-
                   <div class="tab-pane" id="alcohol">
                   <!-- espacio para mostrar los archivos subidos de este cliente -->
                   <p>espacio para mostrar los servicios filtrados por el servicio de alcohol y drogas</p> 
                   </div>
-
                 </div>
               </div>
-
             </div>
           </div>
         </div>
---
+++ Vistas/Paginas/detalleClienteTabPerfil.php
@@ -0,0 +1,34 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col">
+			<label>Address:</label>
+			<input type="" name="" class="form-control" disabled><br>
+
+			<label>LIC EMP #:</label>
+			<input type="" name="" class="form-control" disabled><br>
+
+			<label>PHONE 1:</label>
+			<input type="" name="" class="form-control" disabled><br>
+		</div>
+		<div class="col">
+			<label>PHONE 2 :</label>
+			<input type="" name="" class="form-control" disabled><br>
+
+			<label>E-MAIL 1:</label>
+			<input type="" name="" class="form-control" disabled><br>
+
+			<label>E-MAIL 2:</label>
+			<input type="" name="" class="form-control" disabled><br>
+		</div>
+		<div class="col">
+			<label>ID CGPE:</label>
+			<input type="" name="" class="form-control" disabled><br>
+
+			<label># IFTA:</label>
+			<input type="" name="" class="form-control" disabled><br>
+
+			<label>TAX ID:</label>
+			<input type="" name="" class="form-control" disabled><br>
+		</div>
+	</div>
+</div>
